c846166ab4a8754380e66bd0e46d07cbd62b0af8
Essam Ayman <68829926+EssamAyman115@users.noreply.github.com>
2025-06-09 03:00:52 +0300

    Add project files.

---
+++ Run.py
@@ -0,0 +1,12 @@
+import requests
+
+url = 'http://127.0.0.1:5000/chat'  
+payload = {
+    "title": "الصداع",
+    "question": "علاج الصداع النصفي؟"
+}
+
+response = requests.post(url, json=payload)
+print(response.json())
+
+
